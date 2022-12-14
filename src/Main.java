5f26ec552c41de425c169f4940bff45cd8617cca
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2022-12-14 16:51:33 +0100

    к

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-Person person = new Person();
-Person person1 = new Person("Artsiom", 39, 180, 91);
+Person firstPerson = new Person();
+        firstPerson.age = 18;
+        firstPerson.name = "Kate";
+        firstPerson.height = 195;
+        firstPerson.weight = 67.2;
+Person secondPerson = new Person("Artiom", 39, 180, 91);
 
+        System.out.println(firstPerson);
+        System.out.println();
+        System.out.println(secondPerson);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,10 @@
 public class Person {
     String name;
     int age;
-    double height;
+    int height;
     double weight;
 
-    public Person(String name, int age, double height, double weight ){
+    public Person(String name, int age, int height, double weight ){
         this.name = name;
         this.age = age;
         this.height = height;
@@ -12,7 +12,17 @@ public class Person {
     }
 public Person (){
 };
-
+public void info(int age, String name, int height){
+    System.out.println("The person has" + age + "y.o, " + "his name is " + name + "and" + "his weight is " + height);
+}
+public void goForWork(int age){
+    if(age < 18 || age > 70){
+        System.out.println("I'm resting at home");
+    }
+    else {
+        System.out.println("I'm work");
+    }
+}
 
 
 
