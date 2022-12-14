dc3e8562e02568c67417be4994a800172992c18e
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2022-12-14 15:18:13 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+Person person = new Person();
+Person person1 = new Person("Artsiom", 39, 180, 91);
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    String name;
+    int age;
+    double height;
+    double weight;
+
+    public Person(String name, int age, double height, double weight ){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+public Person (){
+};
+
+
+
+
+
+
+}
